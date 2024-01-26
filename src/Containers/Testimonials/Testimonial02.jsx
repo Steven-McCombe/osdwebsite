619ea2205345d36d87d475c805f8d7297dd462d5
Steven McCombe <smccombe93@gmail.com>
2024-01-26 00:44:07 -0500

    updated other testimonial

--- src/Containers/Testimonials/Testimonial02.jsx
+++ src/Containers/Testimonials/Testimonial02.jsx
@@ -10,55 +10,49 @@ const Testimonial02 = () => {
             id: '1',
             upperImg: 'images/others/u_img_1.png',
             midImg: 'images/others/1.png',
-            name: 'Johnathon Hall'
+            name: 'Johnathon Hall',
+            testimonial: "OSD Builders redefined what I thought was possible in commercial construction. Their team's diligence and skill brought our project to completion ahead of schedule and under budget. Their attention to detail is unparalleled."
         },
         {
             id: '2',
             upperImg: 'images/others/u_img_2.png',
             midImg: 'images/others/2.png',
-            name: 'Madison Koay'
+            name: 'Madison Koay',
+            testimonial: "The level of craftsmanship from OSD Builders is simply outstanding. They transformed my retail space into a vibrant and welcoming environment that customers love. Their design team was exceptional in bringing my ideas to life."
         },
         {
             id: '3',
             upperImg: 'images/others/u_img_3.png',
             midImg: 'images/others/3.png',
-            name: 'Andrew Mark'
+            name: 'Andrew Mark',
+            testimonial: "Working with OSD Builders was an absolute pleasure. Their professionalism, timely communication, and commitment to quality made the renovation process of our downtown office a smooth and stress-free experience."
         },
 
     ];
     return (
         <div className="section testimonial2">
-            {/* <div className="owl_testimonial2 owl-carousel owl-theme"> */}
             <Swiper
-                // className="owl_testimonial2 owl-carousel owl-theme"
                 modules={[Pagination, Controller]}
                 effect={"slide"}
                 slidesPerView={1}
                 loop={true}
                 controller={{ inverse: true }}
-                // spaceBetween={50}
                 centeredSlides={true}
                 pagination={{
-                    // el: '.testimonial-pagination',
                     clickable: true,
-                    // dynamicBullets: true,
                 }}
             >
-
                 {
-                    TestimonialData02.map(data =>
-                        <SwiperSlide>
+                    TestimonialData02.map(data => (
+                        <SwiperSlide key={data.id}>
                             <TestimonialCard02
-                                key={data.id}
                                 data={data}
-                            /></SwiperSlide>)
+                            />
+                        </SwiperSlide>
+                    ))
                 }
-
             </Swiper>
-
-            {/* <!-- Add Pagination --> */}
-            {/* <div className="testimonial-pagination"></div> */}
-        </div >
+        </div>
     );
 };
 
